5e486ef607ac8b986cd4f05dfc399041fcdf520f
Léo <91556140+leochapp@users.noreply.github.com>
2022-04-27 11:40:21 +0200

    Add files via upload

--- suppression_seance.php
+++ suppression_seance.php
@@ -10,11 +10,11 @@ $connect = mysqli_connect($dbhost, $dbuser, $dbpass, $dbname) or die('error conn
 
 // récupération de la date du jour mise dans $aujourdhui
 date_default_timezone_set('europe/paris');
-$aujourdhui = date("y-m-d");
+$aujourdhui = date("y/-m/-d");
 
 //la ligne suivante permet d'éviter les problèmes d'accent entre la page web et le serveur mysql
 mysqli_set_charset($connect, 'utf8'); //les données envoyées vers mysql sont encodées en utf-8
-$result = mysqli_query($connect,"SELECT * FROM seances WHERE supprime=0");
+$result = mysqli_query($connect,"SELECT * FROM seances WHERE supprime = 0");
 /*La ligne du dessus représente la requete qui permet à php de récupérer les données demandés (ici en l'occurence la liste des
 noms qui sont présent dans notre tableau) et de les trier par ordre alphabétique.*/
 //On place sous forme de tableau les données récupérées dans la requête
@@ -29,12 +29,18 @@ else{
   echo "<form method='post' action='supprimer_seance.php'>";
   echo "</select><br><br>";
   echo "<fieldset style='width: 50%;''>";
-  echo "<label for='menuchoixtheme'> Veuillez selectionner la séance à supprimer </label>";
-  echo "<select name='menuchoixtheme' id='menuchoixtheme' size='4' style='width:20%; text-align: center'>";
+  echo "<label for='menuchoixseance_supp'> Veuillez selectionner la séance à supprimer </label>";
+  echo "<select name='menuchoixseance_supp' id='menuchoixseance_supp' size='4' style='width:20%; text-align: center'>";
+
   /*Tant qu'on a des choses qui rentrent dans notre tableau alors on va afficher les noms qu'on récupère dans une balise <select> en html*/
   while($response = mysqli_fetch_array($result)) {
-    echo "<option value=".$response['idtheme'].">".$response['nom']."<br><br>";
+
+    $theme = mysqli_query($connect,"SELECT nom FROM theme WHERE idtheme= '{$response['idtheme']}'");
+
+    echo "<option value=".$response['idseance'].">".$theme.$response['Date Seance']."</option><br><br>";
   }
+
+
   echo "</select><br><br>";
   echo "<input type='submit' value='enregistrer séance'>";
   echo "</form>";
