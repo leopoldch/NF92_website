f337e95c78137544e92e816fea90d6ed4fddf8a6
Léo <91556140+leochapp@users.noreply.github.com>
2022-09-08 10:29:22 +0200

    Update connexion.php

--- connexion.php
+++ connexion.php
@@ -1,9 +1,9 @@
 <?php
 
-        $dbhost = 'tuxa.sme.utc';
-        $dbuser = 'nf92p018';
-        $dbpass = 'vE5DSom3';
-        $dbname = 'nf92p018';
+        $dbhost = '';
+        $dbuser = '';
+        $dbpass = '';
+        $dbname = '';
         $connect = mysqli_connect($dbhost, $dbuser, $dbpass, $dbname) or die ('Error connecting to mysql');
         //la ligne suivante permet d'éviter les problèmes d'accent entre la page web et le serveur mysql
         mysqli_set_charset($connect, 'utf8'); //les données envoyées vers mysql sont encodées en UTF-8
